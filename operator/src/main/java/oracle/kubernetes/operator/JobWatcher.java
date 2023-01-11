6d35ce335ee85485d51075fd14218517580cb79b
anthony_lai <anthony.lai@oracle.com>
2023-01-11 22:57:17 +0000

    jobwatcher messages

--- operator/src/main/java/oracle/kubernetes/operator/JobWatcher.java
+++ operator/src/main/java/oracle/kubernetes/operator/JobWatcher.java
@@ -206,14 +206,22 @@ public class JobWatcher extends Watcher<V1Job> implements WatchListener<V1Job>, 
   public void receivedResponse(Watch.Response<V1Job> item) {
     LOGGER.entering();
 
-    LOGGER.info("JobWatcher.receivedResponse response item: " + item);
+    LOGGER.fine("JobWatcher.receivedResponse response item: " + item);
     switch (item.type) {
       case "ADDED":
+        LOGGER.fine("JobWatcher.receivedResponse  ADDED response item: " + item.object);
+        dispatchCallback(getJobName(item), item.object);
+        break;
       case "MODIFIED":
+        LOGGER.fine("JobWatcher.receivedResponse MODIFIED response item: " + item.object);
         dispatchCallback(getJobName(item), item.object);
         break;
       case "DELETED":
+        LOGGER.fine("JobWatcher.receivedResponse DELETED response item: " + item.object);
+        break;
       case "ERROR":
+        LOGGER.fine("JobWatcher.receivedResponse ERROR response item: " + item.object);
+        break;
       default:
     }
 
