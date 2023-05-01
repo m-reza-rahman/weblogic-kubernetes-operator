6034d85f16cdeaef817ec9fda23813cc77ca77d6
Ryan Eberhard <ryan.eberhard@oracle.com>
2023-05-01 16:20:49 -0400

    Update nginx image

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/NginxParams.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/actions/impl/NginxParams.java
@@ -12,7 +12,9 @@ import oracle.weblogic.kubernetes.actions.impl.primitive.HelmParams;
 import static oracle.weblogic.kubernetes.TestConstants.NGINX_INGRESS_IMAGE_DIGEST;
 import static oracle.weblogic.kubernetes.TestConstants.NGINX_INGRESS_IMAGE_TAG;
 import static oracle.weblogic.kubernetes.TestConstants.TEST_IMAGES_REPO;
+import static oracle.weblogic.kubernetes.TestConstants.TEST_IMAGES_TENANCY;
 import static oracle.weblogic.kubernetes.TestConstants.TEST_NGINX_IMAGE_NAME;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getDomainImagePrefix;
 
 /**
  * All parameters needed to install NGINX ingress controller.
@@ -38,7 +40,7 @@ public class NginxParams {
   private boolean webhooksEnabled = false;
   private HelmParams helmParams;
   private String ingressClassName;
-  private String imageRegistry = TEST_IMAGES_REPO;
+  private String imageRegistry = getDomainImagePrefix(TEST_IMAGES_REPO, TEST_IMAGES_TENANCY);
   private String nginxImage = TEST_NGINX_IMAGE_NAME;
   private String nginxImageTag = NGINX_INGRESS_IMAGE_TAG;
   private String nginxImageDigest = NGINX_INGRESS_IMAGE_DIGEST;
