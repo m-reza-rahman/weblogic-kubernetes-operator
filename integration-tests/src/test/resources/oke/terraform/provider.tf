b8c9116c665961a221d94f5b0bca0eb8844b8fb7
Marina Kogan <marina.kogan@oracle.com>
2023-05-02 22:39:03 +0000

    test

--- integration-tests/src/test/resources/oke/terraform/provider.tf
+++ integration-tests/src/test/resources/oke/terraform/provider.tf
@@ -5,17 +5,21 @@
 */
 
 // These variables would commonly be defined as environment variables or sourced in a .env file
-
-variable "region" {
-  default = "us-phoenix-1"
-}
-
 variable "tenancy_ocid" {
 }
 
 variable "user_ocid" {
 }
 
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+  default = "us-phoenix-1"
+}
 
 terraform {
     required_providers {
@@ -27,7 +31,10 @@ terraform {
 }
 
 provider "oci" {
-   #auth = "InstancePrincipal"
-   region = "${var.region}"
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
 }
 
