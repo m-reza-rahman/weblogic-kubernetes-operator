f2b0010e777c33dc0ed953b222ea2371a15c989a
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2023-05-03 09:55:22 -0700

    remove deleting hostpath provisioner

--- integration-tests/src/test/java/oracle/verrazzano/weblogic/kubernetes/ItVzDBOperator.java
+++ integration-tests/src/test/java/oracle/verrazzano/weblogic/kubernetes/ItVzDBOperator.java
@@ -101,7 +101,6 @@ import static oracle.weblogic.kubernetes.utils.CommonTestUtils.testUntil;
 import static oracle.weblogic.kubernetes.utils.DbUtils.createOracleDBUsingOperator;
 import static oracle.weblogic.kubernetes.utils.DbUtils.createRcuAccessSecret;
 import static oracle.weblogic.kubernetes.utils.DbUtils.createRcuSchema;
-import static oracle.weblogic.kubernetes.utils.DbUtils.deleteHostPathProvisioner;
 import static oracle.weblogic.kubernetes.utils.DbUtils.deleteOracleDB;
 import static oracle.weblogic.kubernetes.utils.DbUtils.installDBOperator;
 import static oracle.weblogic.kubernetes.utils.DbUtils.uninstallDBOperator;
@@ -646,7 +645,6 @@ class ItVzDBOperator {
   public void tearDownAll() throws ApiException {
     if (!SKIP_CLEANUP) {
       deleteOracleDB(dbNamespace, dbName);
-      deleteHostPathProvisioner(dbNamespace);
       uninstallDBOperator(dbNamespace);
     }
   }
