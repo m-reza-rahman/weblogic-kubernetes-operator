c19f8201c3be2e4699fae53fcca4408600917a5b
Marina Kogan <marina.kogan@oracle.com>
2023-06-08 05:15:14 +0000

    added fix

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMonitoringExporterWebApp.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItMonitoringExporterWebApp.java
@@ -216,11 +216,11 @@ class ItMonitoringExporterWebApp {
       assertDoesNotThrow(() -> createPvAndPvc(prometheusReleaseName, monitoringNS, labels, className));
       String command = "chown -R 1000:1000 /data";
       createJobToChangePermissionsOnPvHostPath("pv-test" + prometheusReleaseName,
-          "pvc-test" + prometheusReleaseName, monitoringNS,
+          "pvc-" + prometheusReleaseName, monitoringNS,
           "/data",
            command);
       createJobToChangePermissionsOnPvHostPath("pv-test" + prometheusReleaseName,
-          "pvc-test" + prometheusReleaseName, monitoringNS,
+          "pvc-" + prometheusReleaseName, monitoringNS,
           "/etc/config",
           "chown -R 1000:1000 /etc/config");
 
