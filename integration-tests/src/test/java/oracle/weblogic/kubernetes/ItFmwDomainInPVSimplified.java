b2c216e73ccbdf5942e1046fffc3310382bc6fdc
Marina Kogan <marina.kogan@oracle.com>
2023-07-27 17:51:33 +0000

    splited into 2 classes one for jrf another for wls

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwDomainInPVSimplified.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwDomainInPVSimplified.java
@@ -16,32 +16,18 @@ import java.util.concurrent.Callable;
 
 import io.kubernetes.client.custom.Quantity;
 import io.kubernetes.client.custom.V1Patch;
-import io.kubernetes.client.openapi.ApiException;
-import io.kubernetes.client.openapi.models.V1EnvVar;
-import io.kubernetes.client.openapi.models.V1LocalObjectReference;
-import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import io.kubernetes.client.openapi.models.V1PersistentVolumeClaimVolumeSource;
-import io.kubernetes.client.openapi.models.V1Volume;
-import io.kubernetes.client.openapi.models.V1VolumeMount;
-import oracle.weblogic.domain.AdminServer;
-import oracle.weblogic.domain.AdminService;
-import oracle.weblogic.domain.Channel;
-import oracle.weblogic.domain.ClusterResource;
 import oracle.weblogic.domain.Configuration;
 import oracle.weblogic.domain.CreateIfNotExists;
 import oracle.weblogic.domain.DomainCreationImage;
 import oracle.weblogic.domain.DomainOnPV;
 import oracle.weblogic.domain.DomainOnPVType;
 import oracle.weblogic.domain.DomainResource;
-import oracle.weblogic.domain.DomainSpec;
 import oracle.weblogic.domain.InitializeDomainOnPV;
 import oracle.weblogic.domain.Opss;
-import oracle.weblogic.domain.ServerPod;
 import oracle.weblogic.kubernetes.actions.impl.primitive.HelmParams;
 import oracle.weblogic.kubernetes.actions.impl.primitive.WitParams;
 import oracle.weblogic.kubernetes.annotations.IntegrationTest;
 import oracle.weblogic.kubernetes.annotations.Namespaces;
-import oracle.weblogic.kubernetes.assertions.impl.Cluster;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -50,18 +36,11 @@ import org.junit.jupiter.api.Test;
 
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
-import static oracle.weblogic.kubernetes.TestConstants.BASE_IMAGES_REPO_SECRET_NAME;
-import static oracle.weblogic.kubernetes.TestConstants.CLUSTER_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.DB_IMAGE_TO_USE_IN_SPEC;
-import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_API_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_IMAGES_PREFIX;
 import static oracle.weblogic.kubernetes.TestConstants.ELASTICSEARCH_HOST;
-import static oracle.weblogic.kubernetes.TestConstants.FAILURE_RETRY_INTERVAL_SECONDS;
-import static oracle.weblogic.kubernetes.TestConstants.FAILURE_RETRY_LIMIT_MINUTES;
 import static oracle.weblogic.kubernetes.TestConstants.FMWINFRA_IMAGE_TO_USE_IN_SPEC;
-import static oracle.weblogic.kubernetes.TestConstants.IMAGE_PULL_POLICY;
 import static oracle.weblogic.kubernetes.TestConstants.MII_BASIC_IMAGE_TAG;
-import static oracle.weblogic.kubernetes.TestConstants.OKD;
 import static oracle.weblogic.kubernetes.TestConstants.OKE_CLUSTER;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_CHART_DIR;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_RELEASE_NAME;
@@ -73,10 +52,7 @@ import static oracle.weblogic.kubernetes.actions.impl.Domain.patchDomainCustomRe
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.pvcExists;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.verifyRollingRestartOccurred;
 import static oracle.weblogic.kubernetes.utils.AuxiliaryImageUtils.createAndPushAuxiliaryImage;
-import static oracle.weblogic.kubernetes.utils.ClusterUtils.createClusterAndVerify;
-import static oracle.weblogic.kubernetes.utils.ClusterUtils.createClusterResource;
 import static oracle.weblogic.kubernetes.utils.ClusterUtils.deleteClusterCustomResourceAndVerify;
-import static oracle.weblogic.kubernetes.utils.CommonTestUtils.addSccToDBSvcAccount;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getNextFreePort;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getUniqueName;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.testUntil;
@@ -84,6 +60,7 @@ import static oracle.weblogic.kubernetes.utils.DbUtils.createRcuAccessSecret;
 import static oracle.weblogic.kubernetes.utils.DbUtils.createRcuSchema;
 import static oracle.weblogic.kubernetes.utils.DbUtils.startOracleDB;
 import static oracle.weblogic.kubernetes.utils.DomainUtils.createDomainAndVerify;
+import static oracle.weblogic.kubernetes.utils.DomainUtils.createDomainResourceOnPv;
 import static oracle.weblogic.kubernetes.utils.DomainUtils.deleteDomainResource;
 import static oracle.weblogic.kubernetes.utils.FmwUtils.getConfiguration;
 import static oracle.weblogic.kubernetes.utils.FmwUtils.verifyDomainReady;
@@ -160,8 +137,8 @@ class ItFmwDomainInPVSimplified {
     // start DB
     logger.info("Start DB in namespace: {0}, dbListenerPort: {1}, dbUrl: {2}, dbImage: {3}",
         dbNamespace, dbListenerPort, dbUrl, DB_IMAGE_TO_USE_IN_SPEC);
-    assertDoesNotThrow(() -> setupDB(DB_IMAGE_TO_USE_IN_SPEC, dbNamespace, getNextFreePort(), dbListenerPort),
-        String.format("Failed to setup DB in the namespace %s with dbUrl %s, dbListenerPost %s",
+    assertDoesNotThrow(() -> startOracleDB(DB_IMAGE_TO_USE_IN_SPEC, getNextFreePort(), dbNamespace, dbListenerPort),
+        String.format("Failed to start Oracle DB in the namespace %s with dbUrl %s, dbListenerPost %s",
             dbNamespace, dbUrl, dbListenerPort));
 
     // install operator and verify its running in ready state
@@ -501,96 +478,6 @@ class ItFmwDomainInPVSimplified {
   }
 
   /**
-   * Create a basic WLS domain on PV.
-   * Operator will create PV/PVC and WLS Domain.
-   * Verify Pod is ready and service exists for both admin server and managed servers.
-   */
-  @Test
-  @DisplayName("Create a WLS domain on PV using simplified feature, Operator creates PV/PVC and WLS Domain")
-  void testOperatorCreatesPvPvcWlsDomain() {
-    String domainUid = "wlsonpv-simplified";
-    final String pvName = getUniqueName(domainUid + "-pv-");
-    final String pvcName = getUniqueName(domainUid + "-pvc-");
-    final int t3ChannelPort = getNextFreePort();
-    final String wlSecretName = domainUid + "-weblogic-credentials";
-    final String wlsModelFile = wlsModelFilePrefix + ".yaml";
-
-    // create FMW domain credential secret
-    createSecretWithUsernamePassword(wlSecretName, domainNamespace,
-        ADMIN_USERNAME_DEFAULT, ADMIN_PASSWORD_DEFAULT);
-
-    // create a model property file
-    File wlsModelPropFile = createWdtPropertyFile("wlsonpv-simplified1", RCUSCHEMAPREFIX + "1");
-
-    // create domainCreationImage
-    String domainCreationImageName = DOMAIN_IMAGES_PREFIX + "wls-domain-on-pv-image";
-    // create image with model and wdt installation files
-    WitParams witParams =
-        new WitParams()
-            .modelImageName(domainCreationImageName)
-            .modelImageTag(MII_BASIC_IMAGE_TAG)
-            .modelFiles(Collections.singletonList(MODEL_DIR + "/" + wlsModelFile))
-            .modelVariableFiles(Collections.singletonList(wlsModelPropFile.getAbsolutePath()));
-    createAndPushAuxiliaryImage(domainCreationImageName, MII_BASIC_IMAGE_TAG, witParams);
-
-    DomainCreationImage domainCreationImage =
-        new DomainCreationImage().image(domainCreationImageName + ":" + MII_BASIC_IMAGE_TAG);
-
-    // create opss wallet password secret
-    logger.info("Create OPSS wallet password secret");
-    String opsswalletpassSecretName = domainUid + "-opss-wallet-password-secret";
-    assertDoesNotThrow(() -> createOpsswalletpasswordSecret(
-        opsswalletpassSecretName,
-        domainNamespace,
-        ADMIN_PASSWORD_DEFAULT),
-        String.format("createSecret failed for %s", opsswalletpassSecretName));
-
-    // create a domain resource
-    logger.info("Creating domain custom resource");
-    Map<String, Quantity> pvCapacity = new HashMap<>();
-    pvCapacity.put("storage", new Quantity("2Gi"));
-
-    Map<String, Quantity> pvcRequest = new HashMap<>();
-    pvcRequest.put("storage", new Quantity("2Gi"));
-    Configuration configuration = null;
-    if (OKE_CLUSTER) {
-      configuration = getConfiguration(pvcName, pvcRequest, "oci-fss");
-    } else {
-      configuration = getConfiguration(pvName, pvcName, pvCapacity, pvcRequest, storageClassName,
-      this.getClass().getSimpleName());
-    }
-    configuration.getInitializeDomainOnPV().domain(new DomainOnPV()
-        .createMode(CreateIfNotExists.DOMAIN)
-        .domainCreationImages(Collections.singletonList(domainCreationImage))
-        .domainType(DomainOnPVType.WLS));
-    DomainResource domain = createDomainResourceOnPv(
-        domainUid,
-        domainNamespace,
-        wlSecretName,
-        clusterName,
-        pvName,
-        pvcName,
-        DOMAINHOMEPREFIX,
-        replicaCount,
-        t3ChannelPort,
-        configuration);
-
-    // Set the inter-pod anti-affinity for the domain custom resource
-    setPodAntiAffinity(domain);
-
-    // create a domain custom resource and verify domain is created
-    createDomainAndVerify(domain, domainNamespace);
-
-    // verify that all servers are ready
-    verifyDomainReady(domainNamespace, domainUid, replicaCount, "nosuffix");
-
-    // delete the domain
-    deleteDomainResource(domainNamespace, domainUid);
-    // delete the cluster
-    deleteClusterCustomResourceAndVerify(domainUid + "-" + clusterName,  domainNamespace);
-  }
-
-  /**
    * Create a basic FMW domain on PV.
    * User creates RCU schema, Operator creates PV/PVC and JRF domain
    * The user creates multiple domain initialization images
@@ -879,103 +766,6 @@ class ItFmwDomainInPVSimplified {
         "Failed to write FMW properties file");
 
     return domainPropertiesFile;
-  }
-
-  private DomainResource createDomainResourceOnPv(String domainUid,
-                                                  String domNamespace,
-                                                  String adminSecretName,
-                                                  String clusterName,
-                                                  String pvName,
-                                                  String pvcName,
-                                                  String domainInHomePrefix,
-                                                  int replicaCount,
-                                                  int t3ChannelPort,
-                                                  Configuration configuration) {
-
-    // create a domain custom resource configuration object
-    DomainResource domain = new DomainResource()
-        .apiVersion(DOMAIN_API_VERSION)
-        .kind("Domain")
-        .metadata(new V1ObjectMeta()
-            .name(domainUid)
-            .namespace(domNamespace))
-        .spec(new DomainSpec()
-            .domainUid(domainUid)
-            .domainHome(domainInHomePrefix + domainUid)
-            .domainHomeSourceType("PersistentVolume")
-            .image(FMWINFRA_IMAGE_TO_USE_IN_SPEC)
-            .imagePullPolicy(IMAGE_PULL_POLICY)
-            .imagePullSecrets(Collections.singletonList(
-                new V1LocalObjectReference()
-                    .name(BASE_IMAGES_REPO_SECRET_NAME)))
-            .webLogicCredentialsSecret(new V1LocalObjectReference()
-                .name(adminSecretName))
-            .includeServerOutInPodLog(true)
-            .logHomeEnabled(Boolean.TRUE)
-            .logHome("/shared/" + domNamespace + "/logs/" + domainUid)
-            .dataHome("")
-            .serverStartPolicy("IfNeeded")
-            .failureRetryIntervalSeconds(FAILURE_RETRY_INTERVAL_SECONDS)
-            .failureRetryLimitMinutes(FAILURE_RETRY_LIMIT_MINUTES)
-            .serverPod(new ServerPod() //serverpod
-                .addEnvItem(new V1EnvVar()
-                    .name("JAVA_OPTIONS")
-                    .value("-Dweblogic.StdoutDebugEnabled=false"))
-                .addEnvItem(new V1EnvVar()
-                    .name("USER_MEM_ARGS")
-                    .value("-Djava.security.egd=file:/dev/./urandom"))
-                .addVolumesItem(new V1Volume()
-                    .name(pvName)
-                    .persistentVolumeClaim(new V1PersistentVolumeClaimVolumeSource()
-                        .claimName(pvcName)))
-                .addVolumeMountsItem(new V1VolumeMount()
-                    .mountPath("/shared")
-                    .name(pvName)))
-            .adminServer(new AdminServer() //admin server
-                .adminService(new AdminService()
-                    .addChannelsItem(new Channel()
-                        .channelName("default")
-                        .nodePort(0))
-                    .addChannelsItem(new Channel()
-                        .channelName("T3Channel")
-                        .nodePort(t3ChannelPort))))
-            .configuration(configuration));
-
-    // create cluster resource for the domain
-    String clusterResName  = domainUid + "-" + clusterName;
-    if (!Cluster.doesClusterExist(clusterResName, CLUSTER_VERSION, domainNamespace)) {
-      ClusterResource cluster = createClusterResource(clusterResName,
-          clusterName, domainNamespace, replicaCount);
-      createClusterAndVerify(cluster);
-    }
-    domain.getSpec().withCluster(new V1LocalObjectReference().name(clusterResName));
-
-    return domain;
-  }
-
-  /**
-   * Start Oracle DB instance, create rcu pod and load database schema in the specified namespace.
-   *
-   * @param dbImage image name of database
-   * @param dbNamespace namespace where DB and RCU schema are going to start
-   * @param dbPort NodePort of DB
-   * @param dbListenerPort TCP listener port of DB
-   * @throws ApiException if any error occurs when setting up database
-   */
-  private static synchronized void setupDB(String dbImage, String dbNamespace, int dbPort, int dbListenerPort)
-      throws ApiException {
-    LoggingFacade logger = getLogger();
-    // create pull secrets when running in non Kind Kubernetes cluster
-    // this secret is used only for non-kind cluster
-    createBaseRepoSecret(dbNamespace);
-
-    if (OKD) {
-      addSccToDBSvcAccount("default", dbNamespace);
-    }
-
-    logger.info("Start Oracle DB with dbImage: {0}, dbPort: {1}, dbNamespace: {2}, dbListenerPort:{3}",
-        dbImage, dbPort, dbNamespace, dbListenerPort);
-    startOracleDB(dbImage, dbPort, dbNamespace, dbListenerPort);
   }
 
   private Callable<Boolean> tagImageAndPushIfNeeded(String originalImage, String taggedImage) {
