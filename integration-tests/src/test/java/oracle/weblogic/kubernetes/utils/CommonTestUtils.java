300d1e7255e14e6f122c88b05b6ea7da8c0277ce
Huiling Zhao <huiling.zhao@oracle.com>
2023-08-29 21:37:22 +0000

    Added debug 2

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonTestUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonTestUtils.java
@@ -1228,7 +1228,8 @@ public class CommonTestUtils {
    */
   public static String getHostAndPort(String hostName, int servicePort) {
     LoggingFacade logger = getLogger();
-    String hostAndPort = ((OKD) ? hostName : K8S_NODEPORT_HOST + ":" + servicePort);
+    //String hostAndPort = ((OKD) ? hostName : K8S_NODEPORT_HOST + ":" + servicePort);
+    String hostAndPort = hostName + ":" + servicePort;
     logger.info("hostAndPort = {0} ", hostAndPort);
     return hostAndPort;
   }
