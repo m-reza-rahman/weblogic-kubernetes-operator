52a2e885613bd59159f359335c6412d71bca277e
Huiling Zhao <huiling.zhao@oracle.com>
2023-09-20 02:55:25 +0000

    debug ItLBTwoDomainsTraefik in non-oke env

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonLBTestUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/CommonLBTestUtils.java
@@ -859,16 +859,16 @@ public class CommonLBTestUtils {
    * @param isHostRouting whether it is host routing
    * @param ingressHostName ingress host name
    * @param pathLocation path location in the console url
-   * @param args arguments to determine hostname on OKE or non-OKE env
+   * @param host external IP address on OKE or k8s node IP address on non-OKE env
    */
   public static void verifyAdminServerAccess(boolean isTLS,
                                              int lbNodePort,
                                              boolean isHostRouting,
                                              String ingressHostName,
                                              String pathLocation,
-                                             String... args) {
+                                             String... host) {
     StringBuffer consoleUrl = new StringBuffer();
-    String hostAndPort = (args.length == 0) ? K8S_NODEPORT_HOST + ":" + lbNodePort : args[0];
+    String hostAndPort = OKE_CLUSTER_PRIVATEIP ? host[0] : host[0] + ":" + lbNodePort;
 
     if (isTLS) {
       consoleUrl.append("https://");
