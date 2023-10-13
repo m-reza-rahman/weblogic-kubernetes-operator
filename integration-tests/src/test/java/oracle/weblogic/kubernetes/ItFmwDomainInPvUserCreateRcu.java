92940f803916aec4bf9309942233ae1100d35611
Marina Kogan <marina.kogan@oracle.com>
2023-10-13 20:21:14 +0000

    added dbug

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwDomainInPvUserCreateRcu.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwDomainInPvUserCreateRcu.java
@@ -35,6 +35,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
@@ -99,6 +100,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @IntegrationTest
 @Tag("kind-sequential")
 @Tag("oke-gate")
+@DisabledIfEnvironmentVariable(named = "ARM", matches = "true")
 public class ItFmwDomainInPvUserCreateRcu {
 
   private static String opNamespace = null;
