bd49273ae351eff325ef42513e89d87ef7dfdc5b
Marina Kogan <marina.kogan@oracle.com>
2023-10-18 18:40:05 +0000

    nginx fix

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
@@ -199,8 +199,8 @@ public interface TestConstants {
   public static final String NGINX_CHART_VERSION = "4.0.17";
   public static final String NGINX_INGRESS_IMAGE_DIGEST = ARM
       ? "sha256:e727015a639975f4fc0808b91f9e88a83c60938b640ee6c2f5606ddd779c858d" :
-      "sha256:e727015a639975f4fc0808b91f9e88a83c60938b640ee6c2f5606ddd779c858d";
-  //"sha256:314435f9465a7b2973e3aa4f2edad7465cc7bcdc8304be5d146d70e4da136e51";
+      "sha256:314435f9465a7b2973e3aa4f2edad7465cc7bcdc8304be5d146d70e4da136e51";
+  
   public static final String TEST_NGINX_IMAGE_NAME = TEST_IMAGES_TENANCY + "/test-images/nginx";
   //public static final String TEST_NGINX_IMAGE_NAME = "/nginx";
   public static final String NGINX_INGRESS_IMAGE_TAG = "v1.6.4";
