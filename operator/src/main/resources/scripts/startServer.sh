89ae0b90b99eea1f7eefa72debd7cf667a95283e
Ryan Eberhard <ryan.eberhard@oracle.com>
2023-10-30 15:52:18 -0400

    More diagnostics

--- operator/src/main/resources/scripts/startServer.sh
+++ operator/src/main/resources/scripts/startServer.sh
@@ -22,6 +22,7 @@ trace "Starting WebLogic Server '${SERVER_NAME}'."
 
 ulimit -a
 ulimit -aH
+cat /proc/sys/kernel/pid_max
 
 if [ ${DOMAIN_SOURCE_TYPE} == "FromModel" ]; then
   source ${SCRIPTPATH}/modelInImage.sh
