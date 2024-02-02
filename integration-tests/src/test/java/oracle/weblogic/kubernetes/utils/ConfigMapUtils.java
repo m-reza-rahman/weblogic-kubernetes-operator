4174a550867a674df91e6da8c0c25c91a9c34fe4
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2024-02-02 09:42:52 -0800

    wip

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/ConfigMapUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/ConfigMapUtils.java
@@ -16,7 +16,6 @@ import java.util.concurrent.Callable;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.models.V1ConfigMap;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
-import io.kubernetes.client.util.Yaml;
 import oracle.weblogic.kubernetes.TestConstants;
 import oracle.weblogic.kubernetes.actions.impl.primitive.Kubernetes;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
@@ -90,8 +89,6 @@ public class ConfigMapUtils {
     Map<String, String> data = new HashMap<>();
 
     for (String modelFile : modelFiles) {
-      logger.info("Adding model file {0}", modelFile);
-      assertDoesNotThrow(() -> logger.info(Files.readString(Paths.get(modelFile))));
       addModelFile(data, modelFile);
     }
 
@@ -102,7 +99,6 @@ public class ConfigMapUtils {
     V1ConfigMap configMap = new V1ConfigMap()
         .data(data)
         .metadata(meta);
-    logger.info(Yaml.dump(configMap));
 
     assertTrue(assertDoesNotThrow(() -> createConfigMap(configMap),
         String.format("Create ConfigMap %s failed due to Kubernetes client  ApiException", configMapName)),
