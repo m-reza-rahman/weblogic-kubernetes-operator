199f236a6820db3cc15c75d2e2b370f537ebed8b
vanajamukkara <vanajakshi.mukkara@oracle.com>
2024-02-27 22:57:45 +0000

    fix error

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItIstioCrossDomainTransaction.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItIstioCrossDomainTransaction.java
@@ -552,7 +552,7 @@ class ItIstioCrossDomainTransaction {
             + "action=receive&dest=jms.testAccountingQueue\"", host, istioIngressPort);
 
     logger.info("curl command {0}", curlString);
-    String result = assertDoesNotThrow(() -> exec(curlString, true));
+    ExecResult result = assertDoesNotThrow(() -> exec(curlString, true));
     logger.info("\n HTTP response is \n " + result.stdout());
     logger.info("curl command returned {0}", result.toString());
 
