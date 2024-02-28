00ad8e80c33fa71572c61efdf847ed7c1e293673
vanajamukkara <vanajakshi.mukkara@oracle.com>
2024-02-28 20:32:15 +0000

    Add debug

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItCrossDomainTransaction.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItCrossDomainTransaction.java
@@ -492,6 +492,10 @@ class ItCrossDomainTransaction {
 
     logger.info("curl command {0}", curlString);
 
+    ExecResult result = assertDoesNotThrow(() -> exec(new String(curlString), true));
+    logger.info("\n HTTP response is \n " + result.stdout());
+    logger.info("curl command returned {0}", result.toString());
+
     testUntil(
         () -> exec(new String(curlString), true).stdout().contains("Messages are distributed"),
         logger,
