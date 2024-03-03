41431c82d3cd0d8192d36bd047335982722d90e7
Huiling Zhao <huiling.zhao@oracle.com>
2024-03-03 02:26:22 +0000

    debug ItManagedCoherence

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItManagedCoherence.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItManagedCoherence.java
@@ -4,7 +4,7 @@
 package oracle.weblogic.kubernetes;
 
 import java.io.IOException;
-//import java.nio.file.Files;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -39,10 +39,11 @@ import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.DOMAIN_API_VERSION;
 import static oracle.weblogic.kubernetes.TestConstants.IMAGE_PULL_POLICY;
-//import static oracle.weblogic.kubernetes.TestConstants.INGRESS_CLASS_FILE_NAME;
+import static oracle.weblogic.kubernetes.TestConstants.INGRESS_CLASS_FILE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.KUBERNETES_CLI;
 import static oracle.weblogic.kubernetes.TestConstants.MANAGED_SERVER_NAME_BASE;
 import static oracle.weblogic.kubernetes.TestConstants.OKD;
+import static oracle.weblogic.kubernetes.TestConstants.OKE_CLUSTER;
 import static oracle.weblogic.kubernetes.TestConstants.TEST_IMAGES_REPO_SECRET_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.TRAEFIK_INGRESS_HTTP_HOSTPORT;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.APP_DIR;
@@ -132,10 +133,14 @@ class ItManagedCoherence {
     domainNamespace = namespaces.get(2);
 
     // install and verify Traefik if not running on OKD
-    if (!OKD || (TestConstants.KIND_CLUSTER
-            && TestConstants.WLSIMG_BUILDER.equals(TestConstants.WLSIMG_BUILDER_DEFAULT))) {
-      traefikParams = installAndVerifyTraefik(traefikNamespace, 0, 0, "NodePort");
-      traefikHelmParams = traefikParams.getHelmParams();
+    if (!OKD) {
+      if (OKE_CLUSTER) {
+        traefikHelmParams = installAndVerifyTraefik(traefikNamespace, 0, 0).getHelmParams();;
+      } else if (TestConstants.KIND_CLUSTER
+          && TestConstants.WLSIMG_BUILDER.equals(TestConstants.WLSIMG_BUILDER_DEFAULT)) {
+        traefikParams = installAndVerifyTraefik(traefikNamespace, 0, 0, "NodePort");
+        traefikHelmParams = traefikParams.getHelmParams();
+      }
     }
 
     // install and verify operator
@@ -158,68 +163,6 @@ class ItManagedCoherence {
    * Deploy the GAR file to cluster-2 that has storage enabled
    * Verify that data can be added and stored in the cache and can also be retrieved from cache.
    */
-  @Test
-  @DisplayName("Two cluster domain with a Coherence cluster and test interaction with cache data")
-  void testMultiClusterCoherenceDomain() {
-    // create a DomainHomeInImage image using WebLogic Image Tool
-    String domImage = createAndVerifyDomainImage();
-
-    // create and verify a two-cluster WebLogic domain with a Coherence cluster
-    createAndVerifyDomain(domImage);
-
-    if (OKD) {
-      String cluster1HostName = domainUid + "-cluster-cluster-1";
-
-      final String cluster1IngressHost = createRouteForOKD(cluster1HostName, domainNamespace);
-
-      // test adding data to the cache and retrieving them from the cache
-      boolean testCompletedSuccessfully = assertDoesNotThrow(()
-          -> coherenceCacheTest(cluster1IngressHost, 0), "Test Coherence cache failed");
-      assertTrue(testCompletedSuccessfully, "Test Coherence cache failed");
-    } else {
-
-      Map<String, Integer> clusterNameMsPortMap = new HashMap<>();
-      for (int i = 1; i <= NUMBER_OF_CLUSTERS; i++) {
-        clusterNameMsPortMap.put(CLUSTER_NAME_PREFIX + i, MANAGED_SERVER_PORT);
-      }
-      // clusterNameMsPortMap.put(clusterName, managedServerPort);
-      logger.info("Creating ingress for domain {0} in namespace {1}", domainUid, domainNamespace);
-      createTraefikIngressForDomainAndVerify(domainUid, domainNamespace, 0, clusterNameMsPortMap, true, null,
-          traefikHelmParams.getReleaseName());
-
-      String clusterHostname = domainUid + "." + domainNamespace + ".cluster-1.test";
-      // get ingress service Name and Nodeport
-      String ingressServiceName = traefikHelmParams.getReleaseName();
-      String traefikNamespace = traefikHelmParams.getNamespace();
-
-      int ingressServiceNodePort = assertDoesNotThrow(()
-          -> getServiceNodePort(traefikNamespace, ingressServiceName, "web"),
-          "Getting Ingress Service node port failed");
-      logger.info("Node port for {0} is: {1} :", ingressServiceName, ingressServiceNodePort);
-
-      String command = KUBERNETES_CLI + " get all --all-namespaces";
-      logger.info("curl command to get all --all-namespaces is: {0}", command);
-
-      try {
-        ExecResult result0 = ExecCommand.exec(command, true);
-        logger.info("result is: {0}", result0.toString());
-      } catch (IOException | InterruptedException ex) {
-        ex.printStackTrace();
-      }
-
-      String hostAndPort = getServiceExtIPAddrtOke(ingressServiceName, traefikNamespace) != null
-          ? getServiceExtIPAddrtOke(ingressServiceName, traefikNamespace)
-          : getHostAndPort(clusterHostname, ingressServiceNodePort);
-
-      assertTrue(checkCoheranceApp(clusterHostname, hostAndPort), "Failed to access Coherance App cation");
-      // test adding data to the cache and retrieving them from the cache
-      boolean testCompletedSuccessfully = assertDoesNotThrow(()
-          -> coherenceCacheTest(clusterHostname, ingressServiceNodePort), "Test Coherence cache failed");
-      assertTrue(testCompletedSuccessfully, "Test Coherence cache failed");
-    }
-  }
-
-  /*
   @Test
   @DisplayName("Two cluster domain with a Coherence cluster and test interaction with cache data")
   void testMultiClusterCoherenceDomain() throws IOException {
@@ -297,7 +240,7 @@ class ItManagedCoherence {
         assertTrue(testCompletedSuccessfully, "Test Coherence cache failed");
       }
     }
-  }*/
+  }
 
   private static String createAndVerifyDomainImage() {
     // create image with model files
