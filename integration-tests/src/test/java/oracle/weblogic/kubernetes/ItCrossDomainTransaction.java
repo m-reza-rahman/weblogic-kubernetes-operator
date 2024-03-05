8213848c7dca56f5d1baa9f403154e7e3f703d50
vanajamukkara <vanajakshi.mukkara@oracle.com>
2024-03-05 20:46:52 +0000

    moving exec curl to another method and add debug

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItCrossDomainTransaction.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItCrossDomainTransaction.java
@@ -536,12 +536,9 @@ class ItCrossDomainTransaction {
             hostAndPort);
 
     logger.info("curl command {0}", curlString);
-    ExecResult result = assertDoesNotThrow(() -> exec(new String(curlString), true));
-    logger.info("\n HTTP response is \n " + result.stdout());
-    logger.info("curl command returned {0}", result.toString());
 
     testUntil(
-        () -> exec(new String(curlString), true).stdout().contains("Messages are distributed"),
+        () -> execCurl(curlString).contains("Messages are distributed"),
         logger,
         "local queue to be updated");
     return true;
@@ -733,5 +730,12 @@ class ItCrossDomainTransaction {
 
     logger.info("Executing curl command {0}", curlCmd);
     assertTrue(callWebAppAndWaitTillReady(curlCmd, 60));
+  }
+
+  String execCurl(String curlString) {
+    ExecResult result = assertDoesNotThrow(() -> exec(new String(curlString), true));
+    logger.info("\n HTTP response is \n " + result.stdout());
+    logger.info("curl command returned {0}", result.toString());
+    return result.stdout();
   }
 }
