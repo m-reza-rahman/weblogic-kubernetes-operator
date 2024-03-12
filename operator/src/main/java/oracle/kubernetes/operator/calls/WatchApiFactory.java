ad2928628f4de1918dd5e4d489d768a561b4c25d
Ryan Eberhard <ryan.eberhard@oracle.com>
2024-03-12 08:47:23 -0400

    Read timeout watch

--- operator/src/main/java/oracle/kubernetes/operator/calls/WatchApiFactory.java
+++ operator/src/main/java/oracle/kubernetes/operator/calls/WatchApiFactory.java
@@ -5,7 +5,10 @@ package oracle.kubernetes.operator.calls;
 
 import io.kubernetes.client.common.KubernetesListObject;
 import io.kubernetes.client.common.KubernetesObject;
+import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.util.generic.GenericKubernetesApi;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public interface WatchApiFactory {
   default <A extends KubernetesObject, L extends KubernetesListObject>
@@ -18,7 +21,11 @@ public interface WatchApiFactory {
       extends GenericKubernetesApi<A, L> implements WatchApi<A> {
     public WatchApiImpl(Class<A> apiTypeClass, Class<L> apiListTypeClass,
                              String apiGroup, String apiVersion, String resourcePlural) {
-      super(apiTypeClass, apiListTypeClass, apiGroup, apiVersion, resourcePlural, Client.getInstance());
+      super(apiTypeClass, apiListTypeClass, apiGroup, apiVersion, resourcePlural, getWatchClient(Client.getInstance()));
+    }
+
+    private static ApiClient getWatchClient(ApiClient client) {
+      return client.setHttpClient(client.getHttpClient().newBuilder().readTimeout(0, SECONDS).build());
     }
   }
 
