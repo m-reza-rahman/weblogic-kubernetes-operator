1fee956b8417bd3cf73b5e40db8b0ea2234417a5
Marina Kogan <marina.kogan@oracle.com>
2024-03-21 19:40:38 +0000

    added nfs ver=3

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/PersistentVolumeUtils.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/PersistentVolumeUtils.java
@@ -10,7 +10,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
@@ -132,8 +134,10 @@ public class PersistentVolumeUtils {
     if (OKE_CLUSTER) {
       String fssDir = FSS_DIR[new Random().nextInt(FSS_DIR.length)];
       logger.info("Using FSS PV directory {0}", fssDir);
+      List<String> mountOptions = Collections.singletonList("vers=3");
       v1pv.getSpec()
           .storageClassName("oci-fss")
+          .mountOptions(mountOptions)
           .nfs(new V1NFSVolumeSource()
               .path(fssDir)
               .server(NFS_SERVER)
@@ -217,8 +221,10 @@ public class PersistentVolumeUtils {
       LoggingFacade logger = getLogger();
       logger.info("Using FSS PV directory {0}", fssDir);
       logger.info("Using NFS_SERVER  {0}", NFS_SERVER);
+      List<String> mountOptions = Collections.singletonList("vers=3");
       v1pv.getSpec()
               .storageClassName("oci-fss")
+              .mountOptions(mountOptions)
               .nfs(new V1NFSVolumeSource()
                       .path(fssDir)
                       .server(NFS_SERVER)
