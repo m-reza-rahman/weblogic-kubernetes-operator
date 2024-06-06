f668d51b911851a2a57306597e8c606b61c225ae
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2024-06-05 17:07:24 -0700

    wip

--- operator/src/main/java/oracle/kubernetes/operator/DomainProcessorImpl.java
+++ operator/src/main/java/oracle/kubernetes/operator/DomainProcessorImpl.java
@@ -919,13 +919,13 @@ public class DomainProcessorImpl implements DomainProcessor, MakeRightExecutor {
     if (!domain.isGenerationLaterThanObservedGeneration()) {
       LOGGER.warning("DOMAIN IS MODIFIED and generation is not later than observed");
       LOGGER.warning("!!!!!!!!!!!!!!!!!!!!!");
-      Yaml.dump(domain);
+      LOGGER.info(Yaml.dump(domain));
       LOGGER.warning("!!!!!!!!!!!!!!!!!!!!!");
       return;
     }
     LOGGER.warning("DOMAIN IS MODIFIED and generation is later than observed");
     LOGGER.warning("!!!!!!!!!!!!!!!!!!!!!");
-    Yaml.dump(domain);
+    LOGGER.info(Yaml.dump(domain));
     LOGGER.warning("!!!!!!!!!!!!!!!!!!!!!");
     LOGGER.fine(MessageKeys.WATCH_DOMAIN, domain.getDomainUid());
     createMakeRightOperation(new DomainPresenceInfo(domain))
