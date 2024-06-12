75759eebe55ab278874101ca7ad974d9299adaeb
Marina Kogan <marina.kogan@oracle.com>
2024-06-12 22:27:39 +0000

    fixed style

--- integration-tests/src/test/resources/oke/terraform/okemodule/scripts/kubeconfig_set_credentials.template.sh
+++ integration-tests/src/test/resources/oke/terraform/okemodule/scripts/kubeconfig_set_credentials.template.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2024 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-kubectl config set-credentials "user-${cluster_id_11}" --exec-command="$HOME/bin/token_helper.sh" \
+${KUBERNETES_CLI:-kubectl} config set-credentials "user-${cluster_id_11}" --exec-command="$HOME/bin/token_helper.sh" \
   --exec-arg="ce" \
   --exec-arg="cluster" \
   --exec-arg="generate-token" \
