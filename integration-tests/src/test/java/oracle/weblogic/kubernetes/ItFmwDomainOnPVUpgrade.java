7e7ee2ad67ef47ef2319affa5df91f1fe4207ab1
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2024-09-20 10:30:31 -0700

    adding jrf upgrade tests

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwDomainOnPVUpgrade.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/ItFmwDomainOnPVUpgrade.java
@@ -5,7 +5,13 @@ package oracle.weblogic.kubernetes;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.OffsetDateTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +21,7 @@ import java.util.concurrent.Callable;
 
 import io.kubernetes.client.custom.Quantity;
 import io.kubernetes.client.custom.V1Patch;
+import io.kubernetes.client.openapi.ApiException;
 import oracle.weblogic.domain.Configuration;
 import oracle.weblogic.domain.CreateIfNotExists;
 import oracle.weblogic.domain.DomainCreationImage;
@@ -27,12 +34,14 @@ import oracle.weblogic.kubernetes.actions.impl.primitive.WitParams;
 import oracle.weblogic.kubernetes.annotations.IntegrationTest;
 import oracle.weblogic.kubernetes.annotations.Namespaces;
 import oracle.weblogic.kubernetes.logging.LoggingFacade;
+import oracle.weblogic.kubernetes.utils.ExecResult;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_PASSWORD_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.ADMIN_USERNAME_DEFAULT;
 import static oracle.weblogic.kubernetes.TestConstants.BASE_IMAGES_PREFIX;
@@ -48,12 +57,19 @@ import static oracle.weblogic.kubernetes.TestConstants.OKE_CLUSTER;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_CHART_DIR;
 import static oracle.weblogic.kubernetes.TestConstants.OPERATOR_RELEASE_NAME;
 import static oracle.weblogic.kubernetes.TestConstants.RESULTS_TEMPFILE;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.ITTESTS_DIR;
 import static oracle.weblogic.kubernetes.actions.ActionConstants.MODEL_DIR;
+import static oracle.weblogic.kubernetes.actions.ActionConstants.RESOURCE_DIR;
+import static oracle.weblogic.kubernetes.actions.TestActions.deletePod;
+import static oracle.weblogic.kubernetes.actions.TestActions.execCommand;
+import static oracle.weblogic.kubernetes.actions.TestActions.getDomainCustomResource;
 import static oracle.weblogic.kubernetes.actions.TestActions.imagePull;
 import static oracle.weblogic.kubernetes.actions.TestActions.imageTag;
 import static oracle.weblogic.kubernetes.actions.impl.Domain.patchDomainCustomResource;
+import static oracle.weblogic.kubernetes.assertions.TestAssertions.podDoesNotExist;
 import static oracle.weblogic.kubernetes.assertions.TestAssertions.verifyRollingRestartOccurred;
 import static oracle.weblogic.kubernetes.utils.AuxiliaryImageUtils.createAndPushAuxiliaryImage;
+import static oracle.weblogic.kubernetes.utils.CommonTestUtils.checkPodReadyAndServiceExists;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getNextFreePort;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.getUniqueName;
 import static oracle.weblogic.kubernetes.utils.CommonTestUtils.testUntil;
@@ -61,11 +77,15 @@ import static oracle.weblogic.kubernetes.utils.DbUtils.createOracleDBUsingOperat
 import static oracle.weblogic.kubernetes.utils.DbUtils.startOracleDB;
 import static oracle.weblogic.kubernetes.utils.DomainUtils.createDomainAndVerify;
 import static oracle.weblogic.kubernetes.utils.DomainUtils.createDomainResourceOnPv;
+import static oracle.weblogic.kubernetes.utils.ExecCommand.exec;
+import static oracle.weblogic.kubernetes.utils.FileUtils.copyFileToPod;
+import static oracle.weblogic.kubernetes.utils.FileUtils.replaceStringInFile;
 import static oracle.weblogic.kubernetes.utils.FmwUtils.getConfiguration;
-import static oracle.weblogic.kubernetes.utils.FmwUtils.verifyDomainReady;
 import static oracle.weblogic.kubernetes.utils.ImageUtils.createBaseRepoSecret;
 import static oracle.weblogic.kubernetes.utils.ImageUtils.imageRepoLoginAndPushImageToRegistry;
 import static oracle.weblogic.kubernetes.utils.OperatorUtils.installAndVerifyOperator;
+import static oracle.weblogic.kubernetes.utils.PatchDomainUtils.patchDomainResource;
+import static oracle.weblogic.kubernetes.utils.PodUtils.checkPodReady;
 import static oracle.weblogic.kubernetes.utils.PodUtils.getPodsWithTimeStamps;
 import static oracle.weblogic.kubernetes.utils.PodUtils.setPodAntiAffinity;
 import static oracle.weblogic.kubernetes.utils.SecretUtils.createOpsswalletpasswordSecret;
@@ -102,6 +122,8 @@ class ItFmwDomainOnPVUpgrade {
 
   private final String fmwModelFilePrefix = "model-fmwdomain-onpv-simplified";
   private final String wlsModelFilePrefix = "model-wlsdomain-onpv-simplified";
+  
+  private final String image1412 = "";
 
   /**
    * Assigns unique namespaces for DB, operator and domain.
@@ -157,32 +179,49 @@ class ItFmwDomainOnPVUpgrade {
     // this secret is used only for non-kind cluster
     createBaseRepoSecret(domainNamespace);
   }
-
+  
   /**
-   * Create a basic FMW domain on PV.
-   * Operator will create PV/PVC/RCU/Domain.
-   * Verify Pod is ready and service exists for both admin server and managed servers.
+   * Create a basic FMW domain on PV with server start mode prod
+   * Verify Pod is ready and service exists for both admin
+   * server and managed servers. 
+   * Run the upgrade assistant to upgade the JRF domain 
+   * verify the domain starts and is upgraded to 14.1.2.0.0
    */
   @Test
   @DisabledIfEnvironmentVariable(named = "OKD", matches = "true")
   @DisplayName("Create a FMW domain on PV using simplified feature, Operator creates PV/PVC/RCU/Domain")
   @Tag("gate")
-  void testOperatorCreatesPvPvcRcuDomain() {
-    String domainUid = "jrfonpv-simplified";
-    final String pvName = getUniqueName(domainUid + "-pv-");
-    final String pvcName = getUniqueName(domainUid + "-pvc-");
+  void testUpgradeProductionDomain() {
+    String domainUid = "jrfonpv-production";
+    String domainHome = DOMAINHOMEPREFIX + domainUid;
+    String startMode = "prod";
+    String pvcName = getUniqueName(domainUid + "-pvc-");
+    String rcuSchemaPrefix = "jrfproduction";
+    String fmwModelFile = Paths.get(RESOURCE_DIR, "securemodeupgrade", "jrf-production-upgrade").toString();
+    createDomain(domainUid, startMode, rcuSchemaPrefix, fmwModelFile, pvcName);
+    launchPvHelperPod(domainNamespace, pvcName);
+    copyResponseFile(domainNamespace, dbUrl, rcuSchemaPrefix, domainHome);
+    runUpgradeAssistant(domainNamespace);
+    runUpgradeDomain(domainNamespace, domainHome);
+    deletePvhelperPod(domainNamespace);
+    patchDomain(domainUid, domainNamespace);
+    verifyDomainReady(domainUid, domainNamespace);
+  }
+
+  void createDomain(String domainName, String startMode, String rcuSchemaprefix, String fmwModelFile, String pvcName) {
+    final String pvName = getUniqueName(domainName + "-pv-");
+
     final int t3ChannelPort = getNextFreePort();
-    final String wlSecretName = domainUid + "-weblogic-credentials";
-    final String fmwModelFile = fmwModelFilePrefix + ".yaml";
+    final String wlSecretName = domainName + "-weblogic-credentials";
     // create FMW domain credential secret
     createSecretWithUsernamePassword(wlSecretName, domainNamespace,
         ADMIN_USERNAME_DEFAULT, ADMIN_PASSWORD_DEFAULT);
 
     // create a model property file
-    File fmwModelPropFile = createWdtPropertyFile("jrfonpv-simplified1", RCUSCHEMAPREFIX + "1");
+    File fmwModelPropFile = createWdtPropertyFile(domainName, startMode, rcuSchemaprefix);
 
     // create domainCreationImage
-    String domainCreationImageName = DOMAIN_IMAGES_PREFIX + "jrf-domain-on-pv-image";
+    String domainCreationImageName = DOMAIN_IMAGES_PREFIX + "jrf-upgrade-dci-" + domainName;
     // create image with model and wdt installation files
     WitParams witParams
         = new WitParams()
@@ -196,7 +235,7 @@ class ItFmwDomainOnPVUpgrade {
         = new DomainCreationImage().image(domainCreationImageName + ":" + MII_BASIC_IMAGE_TAG);
 
     // create opss wallet password secret
-    String opsswalletpassSecretName = domainUid + "-opss-wallet-password-secret";
+    String opsswalletpassSecretName = domainName + "-opss-wallet-password-secret";
     logger.info("Create OPSS wallet password secret");
     assertDoesNotThrow(() -> createOpsswalletpasswordSecret(
         opsswalletpassSecretName,
@@ -225,7 +264,7 @@ class ItFmwDomainOnPVUpgrade {
         .opss(new Opss()
             .walletPasswordSecret(opsswalletpassSecretName)));
     DomainResource domain = createDomainResourceOnPv(
-        domainUid,
+        domainName,
         domainNamespace,
         wlSecretName,
         clusterName,
@@ -243,11 +282,11 @@ class ItFmwDomainOnPVUpgrade {
     // create a domain custom resource and verify domain is created
     createDomainAndVerify(domain, domainNamespace);
 
-    // verify that all servers are ready
-    verifyDomainReady(domainNamespace, domainUid, replicaCount, "nosuffix");
+    // verify that all servers are ready    
+    verifyDomainReady(domainName, domainNamespace);
   }
 
-  private File createWdtPropertyFile(String domainName, String rcuSchemaPrefix) {
+  private File createWdtPropertyFile(String domainName, String startMode, String rcuSchemaPrefix) {
 
     // create property file used with domain model file
     Properties p = new Properties();
@@ -258,15 +297,16 @@ class ItFmwDomainOnPVUpgrade {
     p.setProperty("adminUsername", ADMIN_USERNAME_DEFAULT);
     p.setProperty("adminPassword", ADMIN_PASSWORD_DEFAULT);
     p.setProperty("domainName", domainName);
+    p.setProperty("startMode", startMode);
 
     // create a model property file
-    File domainPropertiesFile = assertDoesNotThrow(() ->
-        File.createTempFile(fmwModelFilePrefix, ".properties", new File(RESULTS_TEMPFILE)),
+    File domainPropertiesFile = assertDoesNotThrow(()
+        -> File.createTempFile(fmwModelFilePrefix, ".properties", new File(RESULTS_TEMPFILE)),
         "Failed to create FMW model properties file");
 
     // create the property file
-    assertDoesNotThrow(() ->
-        p.store(new FileOutputStream(domainPropertiesFile), "FMW properties file"),
+    assertDoesNotThrow(()
+        -> p.store(new FileOutputStream(domainPropertiesFile), "FMW properties file"),
         "Failed to write FMW properties file");
 
     return domainPropertiesFile;
@@ -320,4 +360,129 @@ class ItFmwDomainOnPVUpgrade {
     assertTrue(verifyRollingRestartOccurred(podsWithTimeStamps, 1, domainNamespace),
         String.format("Rolling restart failed for domain %s in namespace %s", domainUid, domainNamespace));
   }
+
+  private void launchPvHelperPod(String namespace, String pvcName) {
+    String podName = "pvhelper";
+
+    String script = ITTESTS_DIR + "/../kubernetes/samples/scripts/domain-lifecycle/pv-pvc-helper.sh";
+    String command = "/bin/bash " + script + " -n " + namespace + " -c " + pvcName + " -m /shared -i " + image1412;
+    ExecResult result = null;
+    try {
+      result = exec(new String(command), true);
+      getLogger().info("The command returned exit value: " + result.exitValue()
+          + " command output: " + result.stderr() + "\n" + result.stdout());
+      assertTrue((result.exitValue() == 0), "command returned non zero value");
+    } catch (Exception e) {
+      getLogger().info("Got exception, command failed with errors " + e.getMessage());
+    }
+    checkPodReady(podName, null, namespace);
+  }
+
+  private void copyResponseFile(String namespace, String dbUrl, String rcuSchemaPrefix, String domainHome) {
+    Path jrfResponseTemplateFile = Paths.get(RESOURCE_DIR, "jrfdomainupgrade", "jrfresponse.txt");
+
+    File jrfResponseFile = assertDoesNotThrow(()
+        -> File.createTempFile("jrfresponse", ".txt", new File(RESULTS_TEMPFILE)),
+        "Failed to create JRF upgrade assistant response file");
+    assertDoesNotThrow(() -> Files.copy(jrfResponseTemplateFile, jrfResponseFile.toPath(), REPLACE_EXISTING),
+        "Failed to copy " + jrfResponseTemplateFile.toString());
+    // replace domainHome, dbUrl and rcuSchemaPrefix in JRF response text file
+    assertDoesNotThrow(() -> replaceStringInFile(
+        jrfResponseFile.toString(), "DOMAIN_HOME", domainHome),
+        "Could not modify the DOMAIN_HOME in response file");
+    assertDoesNotThrow(() -> replaceStringInFile(
+        jrfResponseFile.toString(), "DB_CONNECTION_STRING", dbUrl),
+        "Could not modify the DB_CONNECTION_STRING in response file");
+    assertDoesNotThrow(() -> replaceStringInFile(
+        jrfResponseFile.toString(), "RCU_SCHEMA_PREFIX", rcuSchemaPrefix),
+        "Could not modify the RCU_SCHEMA_PREFIX in response file");
+    assertDoesNotThrow(() -> copyFileToPod(namespace,
+        "pvhelper", "",
+        jrfResponseFile.toPath(),
+        Paths.get("/tmp/jrfresponse.txt")),
+        "Copying file to pod failed");
+  }
+
+  private void runUpgradeAssistant(String namespace) {
+    String command = "/u01/oracle/oracle_common/upgrade/bin/ua -response /tmp/jrfresponse.txt -logDir /tmp";
+    try {
+      ExecResult result = execCommand(namespace, "pvhelper", null, true, "/bin/sh", "-c", command);
+      assertTrue((result.exitValue() == 0), "command returned non zero value");
+    } catch (ApiException | IOException | InterruptedException ex) {
+      logger.severe(ex.getMessage());
+    }
+  }
+
+  private void runUpgradeDomain(String namespace, String domainHome) {
+    try {
+      List<String> stringsToWrite = Arrays.asList(
+          "readDomainForUpgrade(\"" + domainHome + "\")",
+          "updateDomain()",
+          "closeDomain()");
+      File wlstScript = assertDoesNotThrow(()
+          -> File.createTempFile("upgradeDomain", ".py", new File(RESULTS_TEMPFILE)),
+          "Failed to create JRF upgrade domain python script file");
+      Files.write(wlstScript.toPath(), stringsToWrite, StandardOpenOption.CREATE);
+      logger.info("Strings written to the file successfully!");
+      assertDoesNotThrow(() -> copyFileToPod(namespace,
+          "pvhelper", "",
+          wlstScript.toPath(),
+          Paths.get("/tmp/" + wlstScript.getName())),
+          "Copying file to pod failed");
+      String command = "/u01/oracle/oracle_common/common/bin/wlst.sh /tmp/" + wlstScript.getName();
+
+      ExecResult result = execCommand(namespace, "pvhelper", null, true, "/bin/sh", "-c", command);
+      assertTrue((result.exitValue() == 0), "command returned non zero value");
+    } catch (ApiException | IOException | InterruptedException ex) {
+      logger.severe(ex.getMessage());
+    }
+  }
+
+  private void deletePvhelperPod(String namespace) {
+    assertDoesNotThrow(() -> deletePod("pvhelper", namespace));
+    testUntil(
+        podDoesNotExist("pvhelper", null, namespace),
+        logger,
+        "{0} to be deleted in namespace {1}",
+        "pvhelper",
+        namespace);
+  }
+
+  private void patchDomain(String domainUid, String namespace) {
+    DomainResource domain = assertDoesNotThrow(() -> getDomainCustomResource(domainUid, namespace),
+        String.format("getDomainCustomResource failed with ApiException when tried to get domain %s in namespace %s",
+            domainUid, namespace));
+    assertNotNull(domain, "Got null domain resource");
+    logger.info("image before update: " + domain.getSpec().getImage());
+
+    String patchStr = "["
+        + "{\"op\": \"replace\", \"path\": \"/spec/image\", \"value\": \"" + image1412 + "\"},"
+        + "{\"op\": \"replace\", \"path\": \"/spec/serverStartPolicy\", \"value\": \"IfNeeded\"}"
+        + "]";
+    logger.info("PatchStr for imageUpdate: {0}", patchStr);
+
+    assertTrue(patchDomainResource(domainUid, namespace, new StringBuffer(patchStr)),
+        "patchDomainCustomResource(imageUpdate) failed");
+
+    domain = assertDoesNotThrow(() -> getDomainCustomResource(domainUid, namespace),
+        String.format("getDomainCustomResource failed with ApiException when tried to get domain %s in namespace %s",
+            domainUid, namespace));
+    assertNotNull(domain, "Got null domain resource after patching");
+    logger.info("image after update: " + domain.getSpec().getImage());
+  }
+
+  private void verifyDomainReady(String domainUid, String namespace) {
+    String adminServerPodName = domainUid + "-admin-server";
+    // verify the admin server service and pod created
+    checkPodReadyAndServiceExists(adminServerPodName, domainUid, namespace);
+
+    String cluster1ManagedServerPodNamePrefix = domainUid + "-managed-server";
+    // verify managed server services and pods are created
+    for (int i = 1; i <= 2; i++) {
+      logger.info("Checking managed server service and pod {0} is created in namespace {1}",
+          cluster1ManagedServerPodNamePrefix + i, namespace);
+      checkPodReadyAndServiceExists(cluster1ManagedServerPodNamePrefix + i, domainUid, namespace);
+    }
+  }
+  
 }
